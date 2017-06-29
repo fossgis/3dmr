84606b2c7739657521099cf5070e884a0a1ad4f6
Pedro Amaro <n101010k@gmail.com>
2017-06-29 16:56:31 +0100

    Implemented the materialized view for the latest revision of each model.

--- mainapp/models.py
+++ mainapp/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from django_pgviews import view as pg
+
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -43,6 +45,64 @@ class Model(models.Model):
     translation_x = models.FloatField(default=0.0)
     translation_y = models.FloatField(default=0.0)
     translation_z = models.FloatField(default=0.0)
+
+    class Meta:
+        app_label = 'mainapp'
+
+class LatestModel(pg.MaterializedView):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    model_id = models.IntegerField()
+    revision = models.IntegerField()
+    title = models.CharField(max_length=32)
+    description = models.CharField(max_length=512)
+    rendered_description = models.CharField(max_length=1024)
+    upload_date = models.DateField(auto_now_add=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    license = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    tags = fields.HStoreField(default={})
+    rotation = models.FloatField(default=0.0)
+    scale = models.FloatField(default=1.0)
+    translation_x = models.FloatField(default=0.0)
+    translation_y = models.FloatField(default=0.0)
+    translation_z = models.FloatField(default=0.0)
+
+    concurrent_index = 'id'
+    sql = """
+        SELECT
+            latest.id AS id,
+            latest.model_id AS model_id,
+            latest.revision AS revision,
+            latest.title AS title,
+            latest.description AS description,
+            latest.rendered_description AS rendered_description,
+            latest.upload_date AS upload_date,
+            latest.latitude AS latitude,
+            latest.longitude AS longitude,
+            latest.license AS license,
+            latest.rotation AS rotation,
+            latest.scale AS scale,
+            latest.translation_x AS translation_x,
+            latest.translation_y AS translation_y,
+            latest.translation_z AS translation_z,
+            latest.author_id AS author_id,
+            latest.tags AS tags
+        FROM mainapp_model latest
+            LEFT JOIN mainapp_model older
+                ON latest.model_id = older.model_id AND
+                   latest.revision < older.revision
+        WHERE older.revision is NULL
+    """
+
+    class Meta:
+        app_label = 'mainapp'
+        db_table = 'mainapp_latestmodel'
+        managed = False
+
+@receiver(post_save, sender=Model)
+def model_saved(sender, action=None, instance=None, **kwargs):
+    LatestModel.refresh(concurrently=True)
 
 class Change(models.Model):
     author = models.ForeignKey(User, models.CASCADE)
