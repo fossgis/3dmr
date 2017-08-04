2f6a7291ad9042ef7217ad3238e229b49fc82471
Pedro Amaro <n101010k@gmail.com>
2017-08-04 12:56:19 +0100

    Fixed bug with negative latitude/longitude on range search.

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -28,8 +28,8 @@ urlpatterns = [
     url(r'^api/category/(?P<category>.*)$', api.lookup_category, name='lookup_category'),
     url(r'^api/author/(?P<username>.*)/(?P<page_id>[0-9]+)$', api.lookup_author, name='lookup_author'),
     url(r'^api/author/(?P<username>.*)$', api.lookup_author, name='lookup_author'),
-    url(r'^api/search/(?P<latitude>[0-9]+(\.[0-9]+)?)/(?P<longitude>[0-9]+(\.[0-9]+)?)/(?P<distance>[0-9]+(\.[0-9]+)?)/(?P<page_id>[0-9]+)$', api.search_range, name='lookup_range'),
-    url(r'^api/search/(?P<latitude>[0-9]+(\.[0-9]+)?)/(?P<longitude>[0-9]+(\.[0-9]+)?)/(?P<distance>[0-9]+(\.[0-9]+)?)$', api.search_range, name='lookup_range'),
+    url(r'^api/search/?(?P<latitude>-?[0-9]+(\.[0-9]+)?)/(?P<longitude>-?[0-9]+(\.[0-9]+)?)/(?P<distance>[0-9]+(\.[0-9]+)?)/(?P<page_id>[0-9]+)$', api.search_range, name='lookup_range'),
+    url(r'^api/search/(?P<latitude>-?[0-9]+(\.[0-9]+)?)/(?P<longitude>-?[0-9]+(\.[0-9]+)?)/(?P<distance>[0-9]+(\.[0-9]+)?)$', api.search_range, name='lookup_range'),
     url(r'^api/search/title/(?P<title>.*)/(?P<page_id>[0-9]+)$', api.search_title, name='search_title'),
     url(r'^api/search/title/(?P<title>.*)$', api.search_title, name='search_title'),
 ]
