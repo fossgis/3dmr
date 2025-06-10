55d1d380f78e64d01f13615f6d5d21e801362c41
Ayush Dhar Dubey <mailaddubey@gmail.com>
2025-06-11 01:36:26 +0530

    upgrades from deprecated url to re_path

--- modelrepository/urls.py
+++ modelrepository/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.urls import include, re_path
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^', include('mainapp.urls')),
-    url(r'', include('social_django.urls', namespace='social'))
+    re_path('', include('mainapp.urls')),
+    re_path('social/', include('social_django.urls', namespace='social')),
 ]
