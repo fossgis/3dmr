30a6fea77b88eef4727a9c33ce3fe60ea284d91e
Ayush Dhar Dubey <mailaddubey@gmail.com>
2025-06-24 23:12:38 +0530

    migrate to glbloader

--- mainapp/static/mainapp/preview.js
+++ mainapp/static/mainapp/preview.js
@@ -1,5 +1,6 @@
-// zip lib, path for other source files
-zip.workerScriptsPath = "/static/mainapp/lib/";
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 function displayPreview(elementId, model_id, revision, options) {
 	var url = "/api/model/" + model_id + "/" + revision;
@@ -8,7 +9,7 @@ function displayPreview(elementId, model_id, revision, options) {
 		options = {};
 
 	var three = initTHREE(elementId, options);
-	loadObjFromZip(url, options, three, onLoad);
+	loadGLB(url, options, three);
 }
 
 function initTHREE(elementId, options) {
@@ -20,7 +21,15 @@ function initTHREE(elementId, options) {
 	if(typeof options['height'] === 'undefined')
 		options['height'] = renderPane.clientHeight;
 
-	var renderer = new THREE.WebGLRenderer();
+	var renderer = new THREE.WebGLRenderer({ antialias: true, alpha: false });
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(options['width'], options['height']);
+	renderer.outputEncoding = THREE.sRGBEncoding;
+	renderer.toneMapping = THREE.ACESFilmicToneMapping;
+	renderer.toneMappingExposure = 1.0;
+	renderer.physicallyCorrectLights = true;
+	renderer.shadowMap.enabled = true;
+	renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 	renderPane.appendChild(renderer.domElement);
 
@@ -30,10 +39,31 @@ function initTHREE(elementId, options) {
 	var camera = new THREE.PerspectiveCamera(75, options['width'] / options['height'], 0.1, 1000);
 	resizeCanvas(renderer, camera, options);
 
-	var controls = new THREE.OrbitControls(camera, renderer.domElement);
+	var controls = new OrbitControls(camera, renderer.domElement);
 
-	var light = new THREE.AmbientLight(0xffffff);
-	scene.add(light);
+	var ambLight = new THREE.AmbientLight(0xffffff, 0.4);
+	scene.add(ambLight);
+
+	const dirLight = new THREE.DirectionalLight(0xffffff, 1.0);
+	dirLight.position.set(5, 10, 7.5);
+	dirLight.castShadow = true;
+	dirLight.shadow.mapSize.width = 2048;
+	dirLight.shadow.mapSize.height = 2048;
+	dirLight.shadow.bias = -0.0001;
+	scene.add(dirLight);
+
+	const hemiLight = new THREE.HemisphereLight(0xffffff, 0x000000, 0.6);
+	scene.add(hemiLight);
+
+	const pointLight = new THREE.PointLight(0xffffff, 1, 100);
+	pointLight.position.set(10, 10, 10);
+	pointLight.castShadow = true;
+	pointLight.shadow.mapSize.width = 2048;
+	pointLight.shadow.mapSize.height = 2048;
+	pointLight.shadow.camera.near = 0.5;
+	pointLight.shadow.camera.far = 500;
+	pointLight.shadow.camera.fov = 50;
+	scene.add(pointLight);
 
 	renderer.render(scene, camera);
 
@@ -45,130 +75,41 @@ function initTHREE(elementId, options) {
 	}
 }
 
-function loadObjFromZip(url, options, three, callback) {
-	zip.createReader(new zip.HttpReader(url), function(reader) {
-		// zip.createReader callback
-		reader.getEntries(function(entries) {
-			var objText;
-			var mtlText;
-			var textures = {};
+function loadGLB(url, options, three) {
+	const loader = new GLTFLoader();
 
-			var counter = Object.keys(entries).length;
-			function updateCounter() {
-				--counter;
-				if(counter == 0)
-					callback(objText, mtlText, textures, options, three);
-			}
+	loader.load(url, function(gltf) {
+		const scene = three.scene;
+		const camera = three.camera;
+		const renderer = three.renderer;
+		const controls = three.controls;
 
-			for(var i in entries) {
-				var entry = entries[i];
-				var name = entry.filename;
-				
-				if(name.endsWith(".obj"))
-					entry.getData(new zip.TextWriter(), function(text) {
-						objText = text;
-						updateCounter();
-					});
-				else if(name.endsWith(".mtl"))
-					entry.getData(new zip.TextWriter(), function(text) {
-						mtlText = text;
-						updateCounter();
-					});
-				else // let's assume it's a texture.
-					(function(name) {
-					entry.getData(new zip.BlobWriter(), function(blob) {
-						textures[name] = (window.webkitURL || window.URL).createObjectURL(blob);
-						updateCounter();
-					});
-					})(name);
+		const object = gltf.scene;
+		scene.add(object);
+
+		object.traverse(function(child) {
+			if (child.isMesh) {
+				child.castShadow = true;
+				child.receiveShadow = true;
 			}
 		});
-	}, function(e) {
-		// zip.createReader on error
-		if(e && e.target && e.target.getAllResponseHeaders) {
-			// if abort is truthy, then the user aborted the downloading.
-			var abort = !e.target.getAllResponseHeaders();
 
-			if(abort) return;
-		}
+		const bbox = new THREE.Box3().setFromObject(object);
+		const center = bbox.getCenter(new THREE.Vector3());
+		const size = bbox.getSize(new THREE.Vector3());
 
-		var scene = three['scene'];
-		var camera = three['camera'];
-		var renderer = three['renderer'];
-		var controls = three['controls'];
-		scene.background = new THREE.Color(0xed4337);
+		object.position.sub(center);
+
+		const maxDim = Math.max(size.x, size.y, size.z);
+		const fov = camera.fov * (Math.PI / 180);
+		const cameraZ = Math.abs(maxDim / 2 / Math.tan(fov / 2));
+		camera.position.set(center.x, center.y, cameraZ * 1.5);
+		camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 		animate(renderer, scene, camera, controls, options);
+	}, undefined, function(error) {
+		console.error("Error loading GLB:", error);
 	});
-}
-
-
-function onLoad(objText, mtlText, textures, options, three) {
-	var scene = three['scene'];
-	var camera = three['camera'];
-	var renderer = three['renderer'];
-	var controls = three['controls'];
-
-	var mtlLoader = new THREE.MTLLoader();
-    mtlLoader.setMaterialOptions({
-        side: THREE.DoubleSide
-    });
-	var materialCreator = mtlLoader.parse(mtlText);
-
-	// turn each loaded texture from the material definitions
-	// into the proper URL we got from the zip blobs
-	var materialsInfo = materialCreator.materialsInfo;
-	for(var i in materialsInfo) {
-		var material = materialsInfo[i];
-
-		if(!material.map_kd)
-			continue;
-
-		var splits = material.map_kd.split(/[\/]/);
-		var filename = splits[splits.length - 1];
-
-		material.map_kd = textures[filename];
-	}
-
-	materialCreator.preload();
-
-	var objLoader = new THREE.OBJLoader();
-	objLoader.setMaterials(materialCreator);
-	var object = objLoader.parse(objText);
-
-	for(var i in object.children) {
-		var mesh = object.children[i];
-
-		if(mesh.type != 'LineSegments')
-			continue;
-
-		if(!mesh.material)
-			continue;
-
-		mesh.material.lights = false;
-	}
-
-	scene.add(object);
-
-	var bbox = new THREE.Box3().setFromObject(object);
-
-	object.position.x = -(bbox.min.x + bbox.max.x)/2;
-	object.position.y = -(bbox.min.y + bbox.max.y)/2;
-	object.position.z = -(bbox.min.z + bbox.max.z)/2;
-
-	var difX = bbox.max.x - bbox.min.x;
-	var difY = bbox.max.y - bbox.min.y;
-	var difZ = bbox.max.z - bbox.min.z;
-
-	var cameraDistanceFactor = 0.9;
-	camera.position.x = object.position.x + difX * cameraDistanceFactor;
-	camera.position.y = object.position.y + difY * cameraDistanceFactor;
-	camera.position.z = object.position.z + difZ * cameraDistanceFactor;
-
-	var cameraLookAt = new THREE.Vector3(0, 0, 0);
-	camera.lookAt(cameraLookAt);
-
-	animate(renderer, scene, camera, controls, options);
 }
 
 function animate(renderer, scene, camera, controls, options) {
