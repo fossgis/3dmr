d18b78c1068c135b1318e4e2690fddf81aab702d
Ayush Dhar Dubey <mailaddubey@gmail.com>
2025-06-26 16:35:46 +0530

    remove get_file and get_filelist api

--- mainapp/api.py
+++ mainapp/api.py
@@ -94,40 +94,6 @@ def get_model(request, model_id, revision=None):
     return response
 
 @any_origin
-def get_filelist(request, model_id, revision=None):
-    if not revision:
-        revision = get_object_or_404(LatestModel, model_id=model_id).revision
-
-    model = get_object_or_404(Model, model_id=model_id, revision=revision)
-
-    if model.is_hidden and not admin(request):
-        raise Http404('Model does not exist.')
-
-    zip_file = ZipFile('{}/{}/{}.zip'.format(MODEL_DIR, model_id, revision))
-
-    response = HttpResponse('\n'.join(zip_file.namelist()), content_type='text/plain')
-    response['Cache-Control'] = 'public, max-age=86400';
-    return response
-
-@any_origin
-def get_file(request, filename, model_id, revision=None):
-    if not revision:
-        revision = get_object_or_404(LatestModel, model_id=model_id).revision
-
-    model = get_object_or_404(Model, model_id=model_id, revision=revision)
-
-    if model.is_hidden and not admin(request):
-        raise Http404('Model does not exist.')
-
-    zip_file = ZipFile('{}/{}/{}.zip'.format(MODEL_DIR, model_id, revision))
-
-    response = FileResponse(zip_file.open(filename))
-    response['Content-Disposition'] = 'attachment; filename={}'.format(filename)
-    response['Content-Type'] = 'application/zip'
-    response['Cache-Control'] = 'public, max-age=86400'
-    return response
-
-@any_origin
 def lookup_tag(request, tag, page_id=1):
     key, value = get_kv(tag)
     models = LatestModel.objects.filter(tags__contains={key: value}).order_by('model_id')
