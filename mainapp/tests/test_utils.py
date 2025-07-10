d5ba15f4c3598436273b863c3cef35ab4009165c
Ayush Dhar Dubey <mailaddubey@gmail.com>
2025-07-10 15:04:04 +0530

    replace obsolete assertEquals with new assetEqual

--- mainapp/tests/test_utils.py
+++ mainapp/tests/test_utils.py
@@ -4,11 +4,11 @@ from mainapp.utils import get_kv
 class TestUtils(SimpleTestCase):
     def test_tag_with_single_equality(self):
         tag = "key=value"
-        self.assertEquals(get_kv(tag), ["key", "value"])
+        self.assertEqual(get_kv(tag), ["key", "value"])
 
     def test_tag_with_multiple_equality(self):
         tag_a = "key=value=value"
         tag_b = "key=value=val=v"
 
-        self.assertEquals(get_kv(tag_a), ["key", "value=value"])
-        self.assertEquals(get_kv(tag_b), ["key", "value=val=v"])
+        self.assertEqual(get_kv(tag_a), ["key", "value=value"])
+        self.assertEqual(get_kv(tag_b), ["key", "value=val=v"])
